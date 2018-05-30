ce04be910f6ba445788522a8dcf4c97527ceb264
UniDomi <32169094+UniDomi@users.noreply.github.com>
2018-05-30 21:09:14 +0200

    fixing data, and made sidebar pretty

--- code/sidebar.js
+++ code/sidebar.js
@@ -121,6 +121,10 @@ function createSidebar() {
   }
 
   function createThemeBlocks(dataf, d1e2) {
+    var mapColour = d3.scaleThreshold()
+      .domain([0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.51, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 1])
+      .range(['#4c0000', '#99000d', '#cb181d', '#ef3b2c', '#fb6a4a', '#fc9272', '#fcbba1', '#fee5d9', "#cce5ff", '#99ccff', '#66b2ff', '#3299ff', '#0080ff', '#0066cc', '#004c99', '#003366', "#00264c"]);
+
     console.log(d1e2)
     var data = dataf.filter(function(d) {
       if (d["D1E2_BEZEICHNUNG"] == d1e2) return d;
@@ -143,8 +147,16 @@ function createSidebar() {
       .attr("width", 1500)
       .attr("height", 40)
       .attr("fill", function(d) {
-        return d.JA_STIMMEN_PROZENT <= 50 ? "#e50000" : '#0066cc'
+        if (d.JA_STIMMEN_PROZENT) {
+          var percentage = d.JA_STIMMEN_PROZENT/100;
+          /*console.log(percentage)*/
+          ;
+          return mapColour((percentage)); //Ja-Stimmen in Prozent
+        } else if (d.id == "9329") {
+          return "white"//"#2299ee"
+        } //if lake fill white/blue
       })
+      .attr("border", "solid black 1px")
       .attr("border", "solid black 1px")
     pollsG
       .append("text")
